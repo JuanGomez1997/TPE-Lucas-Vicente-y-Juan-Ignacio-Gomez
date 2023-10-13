520c7f90fcf260f59986c8facbef3074412a6489
Lucas Vicente <lucasvicente002@gmail.com>
2023-10-12 22:56:37 -0300

    Agregada función URLs semánticas y MVC del login funcional

--- router.php
+++ router.php
@@ -1,16 +1,24 @@
 <?php
 require_once './app/controlador/controladorAut.php';
 
-define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
 $accion = 'login'; //accion por defecto
-if (isset($_GET['accion'])) {
+if (!empty($_GET['accion'])) {
     $accion = $_GET['accion'];
 }
+
+// inicio       ->          controladorInicio->mostrarInicio();
+// login        ->          controladorAut->mostrarLogin();
+// aut          ->          controladorAut->aut();
 
 $params = explode('/', $accion); //parseo la accion de los parametros
     
 switch ($params[0]) {
+    /* case ('inicio'):
+        $controlador = new ControladorInicio();
+        $controlador->mostrarInicio();
+        break; */
 	case ('login'):
         $controlador = new ControladorAut();
 		$controlador->mostrarLogin();
@@ -18,7 +26,8 @@ switch ($params[0]) {
     case 'aut':
         $controlador = new ControladorAut();
         $controlador->aut();
+        break;
     default: 
-        echo "404 Page Not Found";
-        break;    
+        echo '<h1> 404 Page Not Found </h1>';
+        break;
 }
